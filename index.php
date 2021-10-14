a50affb2b215d3209a622e2789ee9c3386bd1aa6
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2021-10-14 22:06:18 +0300

    Move index file

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="">
+    <head>
+        <title>PHP Labs</title>
+        <link rel="stylesheet" href="src/styles/app.css">
+    </head>
+    <body>
+        <nav class="navbar">
+            <ul>
+                <li><a href="src/labs/lab1.php">Lab1</a></li>
+                <li><a href="src/labs/lab2.php">Lab2</a></li>
+                <li><a href="src/labs/lab3.php">Lab3</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
