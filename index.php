d7b3f99dd32ea7938363876e905e6970e38b082a
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2021-10-15 00:09:03 +0300

    update index file

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
                 <li><a href="src/labs/lab1.php">Lab1</a></li>
                 <li><a href="src/labs/lab2.php">Lab2</a></li>
                 <li><a href="src/labs/lab3.php">Lab3</a></li>
+                <li><a href="src/labs/lab4.php">Lab4</a></li>
             </ul>
         </nav>
     </body>
