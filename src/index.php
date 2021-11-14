7a1acbc067b7617258f1530454b40238f5a2b5fc
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2021-11-15 00:06:15 +0200

    lab5

--- src/index.php
+++ src/index.php
@@ -5,11 +5,11 @@
     <body>
         <nav class="navbar">
             <ul>
-                <li><a href="labs/lab1.php">Lab 1</a></li>
-                <li><a href="labs/lab2.php">Lab 2</a></li>
-                <li><a href="labs/lab3.php">Lab 3</a></li>
-                <li><a href="labs/lab4.php">Lab 4</a></li>
-                <li><a href="labs/lab5.php">Lab 5</a></li>
+                <li><a href="src/labs/lab1.php">Lab 1</a></li>
+                <li><a href="src/labs/lab2.php">Lab 2</a></li>
+                <li><a href="src/labs/lab3.php">Lab 3</a></li>
+                <li><a href="src/labs/lab4.php">Lab 4</a></li>
+                <li><a href="src/labs/lab5.php">Lab 5</a></li>
             </ul>
         </nav>
     </body>
