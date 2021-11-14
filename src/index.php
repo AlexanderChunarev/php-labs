d29beaf2955d458b9768a9927d5ef42a821a8775
oleksandr.chunarov <alexanderchunarov1999@gmail.com>
2021-11-14 23:11:56 +0200

    lab 5

--- src/index.php
+++ src/index.php
@@ -9,6 +9,7 @@
                 <li><a href="labs/lab2.php">Lab2</a></li>
                 <li><a href="labs/lab3.php">Lab3</a></li>
                 <li><a href="labs/lab4.php">Lab4</a></li>
+                <li><a href="labs/lab5.php">Lab5</a></li>
             </ul>
         </nav>
     </body>
