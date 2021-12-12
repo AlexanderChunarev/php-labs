18af342d1edc226dd122694441ca7e5558e3cd39
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2021-12-12 18:17:40 +0200

    lab 7

--- src/labs/lab7.php
+++ src/labs/lab7.php
@@ -1,17 +1,21 @@
 <html lang="en">
 <head>
     <title>PHP Labs</title>
-    <link rel="stylesheet" href="../styles/lab2.css">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../styles/lab7.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+            integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
 <h1>Lab 7</h1>
-<form action="">
-    <label for="search-field" >Пошук</label>
-    <input type="text" id="search-field" onkeyup="startSearch(this.value)">
+<form class="form" onsubmit="" id="form">
+    <label for="search-field">Search: </label>
+    <input type="text" id="search-field">
+    <button type="submit">Search</button>
 </form>
-<div id="Result" class="container"></div>
-<script src="search.js" ></script>
+<div>
+    <div id="products"></div>
+</div>
+<script src="lab7.js"></script>
 </body>
 </html>
--- src/labs/lab7_script.php
+++ src/labs/lab7_script.php
@@ -12,20 +12,9 @@ $dom->loadHTML($html);
 $xpath = new DOMXpath($dom);
 
 $search_result = $xpath->query("//div[@class='tile product']");
-
 for ($i = 1, $iMax = count($search_result) - 1; $i <= $iMax; $i++) {
     $img = $xpath->query("//*[@class='image']//img", $search_result[$i])[$i]->getAttribute('src');
     $footer = $xpath->query("//div[@class='footer']//a[@data-product]", $search_result[$i])[$i]->nodeValue;
     $description = $xpath->query("//*[@class='footer']/span[1]", $search_result[$i])[$i]->nodeValue;
-    echo "
-            <div class='product-item'>
-                <img src='https://cityshop.net.ua/$img'>
-                <div class='product-item-info'>
-                    <h3>$footer</h3>
-                    <p>$description</p>
-                </div>
-            </div>
-        ";
+    echo "<div class='product-item'><img src='https://cityshop.net.ua/$img'><div class='product-item-info'><h3>$footer</h3><p>$description</p></div></div>";
 }
-
-?>
